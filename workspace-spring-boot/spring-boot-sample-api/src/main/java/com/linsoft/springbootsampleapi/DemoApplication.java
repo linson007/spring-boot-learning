d2c20cb4136f61c8c3dea8c8b68ba715d51228ea
Linson Kurian <linson.kurian@imageinfosystems.com>
2020-06-15 21:53:33 +0530

    basic notes and simple base project

---
+++ workspace-spring-boot/spring-boot-sample-api/src/main/java/com/linsoft/springbootsampleapi/DemoApplication.java
@@ -0,0 +1,16 @@
+package com.linsoft.springbootsampleapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		SpringApplication.run(DemoApplication.class, args);
+		
+	}
+
+}
